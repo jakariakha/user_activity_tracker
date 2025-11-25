04ef584508e32ef7e6f4938f54193269455bafbf
Jakaria <jakariakhan0292@gmail.com>
2025-11-25 21:25:41 +0600

    updated

--- track_commands.sh
+++ track_commands.sh
@@ -3,6 +3,6 @@
 LOGFILE="/var/log/user-activity-tracker.log"
 TS=$(date +"%d-%m-%Y %H:%M:%S")
 USER_NAME=$(whoami)
-CMD=$(history 1 | sed 's/^[ ]*[0-9]\+[ ]*//')
+CMD="command name"
 
 echo "$TS | $USER_NAME | $CMD" >> "$LOGFILE"
